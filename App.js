5a8895a83b64f31bb9a7159076fcc48c26297a9f
Brandon Thompson <thompsonbrandon13@yahoo.com>
2021-03-23 20:32:03 -0500

    added a button

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './assets/logo.png';
 
 export default function App() {
@@ -10,7 +10,10 @@ export default function App() {
       <Text style ={styles.instructions}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>
-      <StatusBar style="auto" />
+      
+      <TouchableOpacity onPress={() => alert('Hello, World!')} style={styles.button}>
+        <Text style ={styles.buttonText}>Pick a photo </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -31,5 +34,14 @@ const styles = StyleSheet.create({
     color: '#888',
     fontSize: 18,
     marginHorizontal: 15,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   },
 });
