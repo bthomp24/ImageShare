0c19dc360848c6b913230974a0da3fa8679b463e
Brandon Thompson <thompsonbrandon13@yahoo.com>
2021-03-25 17:05:42 -0500

    added comments

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './assets/logo.png';
@@ -9,33 +8,42 @@ export default function App() {
 const [selectedImage, setSelectedImage] = React.useState(null);
 
   let openImagePickerAsync = async () => {
-    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    //ask for permission to camera roll
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync(); 
 
+    // if user denies access, show alert
     if (permissionResult.granted === false) {
       alert("Permission to access camera roll is required!");
       return;
     }
-
+    
+    // otherwise launch photo library
     let pickerResult = await ImagePicker.launchImageLibraryAsync();
     
+    // if user cancells, just return
     if (pickerResult.cancelled === true) {
       return;
     }
 
+    // set the selected image
     setSelectedImage({ localUri: pickerResult.uri });
   };
 
   let openShareDialogAsync = async () => {
+
+    // check if sharing is allowed on platform, show alert if not available
     if (!(await Sharing.isAvailableAsync())) {
       alert(`Uh oh, sharing isn't available on your platform`);
       return;
     }
 
+    // open sharing on device
     await Sharing.shareAsync(selectedImage.localUri);
   };
 
   if (selectedImage !== null) {
     return (
+      //selected image screen view
       <View style = {styles.container}>
         <Image source = {{ uri: selectedImage.localUri }} style = {styles.thumbnail}/>
         <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
@@ -46,6 +54,7 @@ const [selectedImage, setSelectedImage] = React.useState(null);
   }
 
   return (
+    // home screen view
     <View style={styles.container}>
       <Image source={logo} style={styles.logo} />
       <Text style ={styles.instructions}>
