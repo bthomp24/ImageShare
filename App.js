81175b62ab13ed7594c1d33766777e6f0c06543b
Brandon Thompson <thompsonbrandon13@yahoo.com>
2021-03-25 16:35:42 -0500

    select and display image

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import logo from './assets/logo.png';
 import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+const [selectedImage, setSelectedImage] = React.useState(null);
+
   let openImagePickerAsync = async () => {
     let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
@@ -14,7 +16,23 @@ export default function App() {
     }
 
     let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    console.log(pickerResult);
+    
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style = {styles.container}>
+        <Image
+          source = {{ uri: selectedImage.localUri }}
+          style = {styles.thumbnail}
+        />
+      </View>
+    );
   }
 
   return (
@@ -56,5 +74,10 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 20,
     color: '#fff',
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
   },
 });
